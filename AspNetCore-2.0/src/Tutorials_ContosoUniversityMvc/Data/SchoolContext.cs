2fd7dd5a0c0697252eb969d1bf58e3138c611932
Ferdinand Lukasak <feroalien@hotmail.com>
2017-12-12 16:36:33 +1000

    Created new project for contoso university mvc sample

---
+++ AspNetCore-2.0/src/Tutorials_ContosoUniversityMvc/Data/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tutorials_ContosoUniversityMvc.Models;
+
+namespace Tutorials_ContosoUniversityMvc.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
