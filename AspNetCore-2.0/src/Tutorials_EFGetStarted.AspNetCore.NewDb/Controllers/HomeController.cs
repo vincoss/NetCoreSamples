f9a793dfd28007a1b62cc76dc803d5b66c405017
Ferdinand Lukasak <feroalien@hotmail.com>
2019-02-27 13:21:22 +1000

    Tutorial EF Core with MVC new DB

---
+++ AspNetCore-2.0/src/Tutorials_EFGetStarted.AspNetCore.NewDb/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tutorials_EFGetStarted.AspNetCore.NewDb.Models;
+
+namespace Tutorials_EFGetStarted.AspNetCore.NewDb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNetCore-2.0/src/Tutorials_EFGetStarted.AspNetCore.NewDb/Readme.txt
@@ -0,0 +1,6 @@
+﻿# Based on
+https://docs.microsoft.com/en-us/ef/core/get-started/aspnetcore/new-db?view=aspnetcore-2.2&tabs=visual-studio
+
+# Migrations
+Add-Migration InitialCreate
+Update-Database
