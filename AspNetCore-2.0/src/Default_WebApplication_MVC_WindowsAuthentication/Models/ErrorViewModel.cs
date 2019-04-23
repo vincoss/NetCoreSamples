67711aac87c9964d500ac063c23823898ee8efb3
Ferdinand Lukasak <feroalien@hotmail.com>
2019-04-23 12:28:33 +1000

    Default web application samples update

---
+++ AspNetCore-2.0/src/Default_WebApplication_MVC_WindowsAuthentication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Default_WebApplication_MVC_WindowsAuthentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
