d0ed95c72dd0129d528b35ca3d3147c4b05afd98
Ferdinand Lukasak <Ferdinand@gad.local>
2019-05-03 14:07:10 +1000

    Custom routing sample attribute

---
+++ AspNetCore-2.0/src/WebApps_Mvc_Routing/Attributes/CountrySpecificAttribute.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApps_Mvc_Routing.Attributes
+{
+    public class CountrySpecificAttribute : Attribute, IActionConstraint
+    {
+        private readonly string _countryCode;
+
+        public CountrySpecificAttribute(string countryCode)
+        {
+            _countryCode = countryCode;
+        }
+
+        public int Order
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public bool Accept(ActionConstraintContext context)
+        {
+            return string.Equals(
+                context.RouteContext.RouteData.Values["country"].ToString(),
+                _countryCode,
+                StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
