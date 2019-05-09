171dd455534127c2a7a359499ab60a603092ea47
Ferdinand Lukasak <feroalien@hotmail.com>
2019-05-09 12:30:29 +1000

    Blazor default projects

---
+++ AspNetCore-2.0/src/Default_Blazor_ClientSide/Startup.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Default_Blazor_ClientSide
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
---
+++ AspNetCore-2.0/src/Default_Blazor_ServerSide/Data/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Default_Blazor_ServerSide.Data
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
