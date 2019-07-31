ecd92c61f6aa27c40d0c7c78e0340f584486b45c
Ferdinand Lukasak <feroalien@hotmail.com>
2019-07-31 16:53:15 +1000

    Test,debug,troubleshoot samples

---
+++ AspNetCore-2.0/src/TestDebugTroubleshoot_DotNetUnitTest/PrimeServicesTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TestDebugTroubleshoot_DotNetUnitTest
+{
+    public    class PrimeServicesTest
+    {
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var service = new PrimeService();
+            var result = service.IsPrime(1);
+
+            Assert.False(result, "1 should not be prime");
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(0)]
+        [InlineData(1)]
+        public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+        {
+            var service = new PrimeService();
+            var result = service.IsPrime(value);
+
+            Assert.False(result, $"{value} should not be prime");
+        }
+    }
+}
---
+++ AspNetCore-2.0/src/TestDebugTroubleshoot_RazorPages/Data/EventInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestDebugTroubleshoot_RazorPages.Data
+{
+    public class EventInfo
+    {
+        [Key]
+        public string Name { get; set; }
+    }
+}
