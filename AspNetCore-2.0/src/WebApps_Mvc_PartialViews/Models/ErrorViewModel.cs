4e62bb2d90b81791008d57e05533e9f214bc9bd8
Ferdinand Lukasak <feroalien@hotmail.com>
2019-08-06 16:56:37 +1000

    Partial view samples Razor & Mvc

---
+++ AspNetCore-2.0/src/WebApps_Mvc_PartialViews/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApps_Mvc_PartialViews.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ AspNetCore-2.0/src/WebApps_Mvc_PartialViews/Pages/IndexPage.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApps_Mvc_PartialViews.Pages
+{
+    public class IndexPageModel : PageModel
+    {
+
+        public void OnGet() => Page();
+
+        public IActionResult OnGetPartial() =>
+            Partial("_AuthorPartial");
+
+    }
+}
