28bfc886fc03dddb2fcb7b1546cef2350fe2f19e
Ferdinand Lukasak <feroalien@hotmail.com>
2019-08-16 15:33:14 +1000

    LibMan project fix, unload Blazor sample too many changes, needs to fix later

---
+++ AspNetCore-2.0/src/WebApps_RazorSyntax_V3/Pages/Ajax/RazorPageAjaxSamples.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApps_RazorSyntax_V3.Models;
+
+namespace WebApps_RazorSyntax_V3.Pages.Ajax
+{
+    public class RazorPageAjaxSamples : PageModel
+    {
+        public RazorPageAjaxSamples()
+        {
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+
+        public JsonResult OnGetFilter(string search)
+        {
+            return new JsonResult(_filterService.GetFilterDropDownValues(filterBy));
+        }
+    }
+}
