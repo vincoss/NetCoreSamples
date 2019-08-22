1376755a89aa1cdab4fbfd3e8456221178904ae7
Ferdinand Lukasak <feroalien@hotmail.com>
2019-08-22 14:04:19 +1000

    API_V3 sample update

--- AspNetCore-2.0/src/Default_WebApplication_API_V3/Startup.cs
+++ AspNetCore-2.0/src/Default_WebApplication_API_V3/Startup.cs
@@ -35,11 +35,6 @@ namespace Default_WebApplication_API_V3
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
 
