53e0fc66c07ed069d63c52110ce5329f493883a6
Ferdinand Lukasak <feroalien@hotmail.com>
2019-08-30 16:42:26 +1000

    Line chart example

---
+++ AspNetCore-2.0/src/WebApps_jQuery_Samples/Pages/ChartsjsLine.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+
+namespace WebApps_jQuery_Samples.Pages
+{
+    public class ChartsjsLine : PageModel
+    {
+        public ChartsjsLine()
+        {
+        }
+
+        public string ChartData { get; set; }
+
+    }
+}
