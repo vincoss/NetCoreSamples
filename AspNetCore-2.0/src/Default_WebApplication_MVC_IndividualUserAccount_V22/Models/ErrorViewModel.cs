130a6006ac0ca83a3daae949fe9ace883d5eda8a
fero <feroalien@hotmail.com>
2020-02-26 12:43:01 +1000

    sample v2.2 user accounts

---
+++ AspNetCore-2.0/src/Default_WebApplication_MVC_IndividualUserAccount_V22/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Default_WebApplication_MVC_IndividualUserAccount_V22.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
