e36509ea0417ea257052cce665d15fe5d7bea79f
fero <feroalien@hotmail.com>
2023-02-13 16:42:53 +1000

    default project

---
+++ AspNetCore-2.0/src/Default_AspNetCoreWebApp_7/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Default_AspNetCoreWebApp_7.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
